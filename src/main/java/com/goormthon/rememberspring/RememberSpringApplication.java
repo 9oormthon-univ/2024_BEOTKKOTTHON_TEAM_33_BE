d6103fc2c31730d2c70027f53c35b1498c839b38
giwoong <dev.choi0409@gmail.com>
2024-03-13 09:25:47 +0900

    first setting

---
+++ src/main/java/com/goormthon/rememberspring/RememberSpringApplication.java
@@ -0,0 +1,13 @@
+package com.goormthon.rememberspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememberSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RememberSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goormthon/rememberspring/RememberSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormthon.rememberspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RememberSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
